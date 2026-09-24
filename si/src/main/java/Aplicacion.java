f714a4dbecfdccd4d592d9bbfe540b66b3bc9a71
christVisan7-svg <cristopher.sanchezv@uqvirtual.edu.co>
2026-09-24 15:58:32 -0500

    case 13

--- si/src/main/java/Aplicacion.java
+++ si/src/main/java/Aplicacion.java
@@ -336,7 +336,7 @@ public class Aplicacion {
                     }
                     break;
                 case 13:
-                    System.out.println("Confirmando ");
+                    System.out.println("Confirmando compra... ");
                     break;
                 case 14:
                     System.out.println("------Mostrar todas las compras------");
