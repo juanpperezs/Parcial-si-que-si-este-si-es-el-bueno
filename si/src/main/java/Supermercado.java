cdb529837556bbe0143569d5a89cbca150d909fe
christVisan7-svg <cristopher.sanchezv@uqvirtual.edu.co>
2026-09-24 12:41:39 -0500

    Metodo mostrar p

--- si/src/main/java/Supermercado.java
+++ si/src/main/java/Supermercado.java
@@ -122,6 +122,11 @@ public class Supermercado {
         return estaActualizado;
     }
 
+    //Metodo mostrar productos
+    public boolean mostrarProducto (int codigoProducto, String nombreProducto, int precioUnitario, int cantidadDisponible){
+
+    }
+
     //Getters y setters
     public String getNombreComercial() {
         return nombreComercial;
