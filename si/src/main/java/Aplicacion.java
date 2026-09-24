3b998ca58e1ddeb759702834b2c339b736b22105
Jeronimo-Gomez <gomeza.jeronimo@uqvirtual.edu.co>
2026-09-24 14:03:01 -0500

    Clase producto

--- si/src/main/java/Aplicacion.java
+++ si/src/main/java/Aplicacion.java
@@ -185,6 +185,7 @@ public class Aplicacion {
                         }
                     }
                     break;
+
                 case 7:
                     System.out.println("Actualizando producto...");
 
@@ -230,6 +231,7 @@ public class Aplicacion {
                         }
                     }
                     break;
+
                 case 8:
                     System.out.println("Eliminando producto...");
 
@@ -242,29 +244,46 @@ public class Aplicacion {
                         System.out.println("Este producto no existe.");
                     }
                     break;
+
                 case 9:
+
                     break;
+
                 case 10:
+
                     break;
+
                 case 11:
+
                     break;
+
                 case 12:
+
                     break;
+
                 case 13:
+
                     break;
+
                 case 14:
                     System.out.println("------Mostrar todas las compras------");
                     for(Compras comprasLista: supermercado.getListaCompras()){
                         System.out.println(comprasLista);
                     }
                     break;
+
                 case 15:
+
                     break;
+
                 case 16:
+
                     break;
+
                 case 0:
                     System.out.println("Finalizando programa...");
                     break;
+
                 default:
                     System.out.println("Opción no válida...");
             }
