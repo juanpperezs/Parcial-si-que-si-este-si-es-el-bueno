da866189ceb96739e17268c6bae006330050edcc
Jeronimo-Gomez <gomeza.jeronimo@uqvirtual.edu.co>
2026-09-23 22:38:17 -0500

    Clase producto

--- si/src/main/java/Arte.java
+++ si/src/main/java/Arte.java
@@ -1,5 +1,5 @@
 public class Arte {
     static void main() {
         System.out.println("No soy gay");
-    }
+
 }
---
+++ si/src/main/java/Producto.java
@@ -0,0 +1,4 @@
+public class Producto {
+    private String codigo;
+    private String nombre;
+}
