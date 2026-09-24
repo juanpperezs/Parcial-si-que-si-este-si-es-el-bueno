543f8204e8b2d4f44f9d40b1589346f2a0237466
christVisan7-svg <cristopher.sanchezv@uqvirtual.edu.co>
2026-09-24 12:22:21 -0500

    Metodo actualizar cliente Puesto en el Menú

--- si/src/main/java/Aplicacion.java
+++ si/src/main/java/Aplicacion.java
@@ -8,6 +8,9 @@ public class Aplicacion {
 
         Supermercado supermercado= new Supermercado("MarketPlus", "Carrera 18", 314678);
 
+        Clientes cliente1= new Clientes("Cesar Augusto", 10934,319717, "crisansi");
+        supermercado.agregarClientes(cliente1);
+
         int opcion;
         do{
             System.out.println("----Menú MARKETPLUS----");
@@ -32,8 +35,6 @@ public class Aplicacion {
                     //Agregar una decision para terminar el sistema con el documento
                     System.out.print("Introduzca el número de teléfono: ");
                     int telefono = sc.nextInt();
-
-                    sc.nextLine();
 
                     System.out.print("Introduzca el correo electrónico: ");
                     String correo = sc.nextLine();
