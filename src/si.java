7b62a201f82b93c6795b53822f59f716447b723c
Juan Pablo Pérez Saavedra <juanp.perezs1@uqvirtual.edu.co>
2026-09-23 22:21:45 -0500

    owo

--- src/si.java
+++ src/si.java
@@ -2,5 +2,7 @@ public class si {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Ayuda por favor");
+        System.out.println("No se que estoy haciendo");
+        System.out.println("no");
     }
 }
