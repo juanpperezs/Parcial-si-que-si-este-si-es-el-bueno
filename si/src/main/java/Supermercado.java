a4169de40ab446a1f2f2ac472eb943aa9892cfd0
christVisan7-svg <cristopher.sanchezv@uqvirtual.edu.co>
2026-09-23 23:01:38 -0500

    Supermercado ya

--- si/src/main/java/Supermercado.java
+++ si/src/main/java/Supermercado.java
@@ -1,7 +1,52 @@
 public class Supermercado {
-    static void main() {
-        System.out.println("Holaaaa");
-        System.out.println("oña");
-        System.out.println("nyejeje");
+
+    //Set atributos
+    private String nombreComercial;
+    private String direccion;
+    private int telefono;
+
+    //Constructor
+    public Supermercado(String nombreComercial, String direccion, int telefono) {
+        this.nombreComercial = nombreComercial;
+        this.direccion = direccion;
+        this.telefono = telefono;
     }
+
+    //Getters y setters
+
+    public String getNombreComercial() {
+        return nombreComercial;
+    }
+
+    public void setNombreComercial(String nombreComercial) {
+        this.nombreComercial = nombreComercial;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    //To string
+    @Override
+    public String toString() {
+        return "Supermercado{" +
+                "nombreComercial='" + nombreComercial + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", telefono=" + telefono +
+                '}';
+    }
+
 }
+
